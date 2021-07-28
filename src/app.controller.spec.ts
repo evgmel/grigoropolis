4c8d29a00fbbfd1246011ce09260b79516567f7e
EvgMel <melnikov_evg@mail.ru>
2021-07-28 13:49:48 +0300

    chore: installed faker

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as faker from 'faker';
 
 describe('AppController', () => {
   let appController: AppController;
+  const userName = faker.name.firstName();
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -15,8 +17,12 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!" when no name supplied', () => {
+      expect(appController.getHello(undefined)).toBe('Hello World!');
+    });
+
+    it(`should return "Hello ${userName}!" when a name supplied`, () => {
+      expect(appController.getHello(userName)).toBe(`Hello ${userName}!`);
     });
   });
 });
