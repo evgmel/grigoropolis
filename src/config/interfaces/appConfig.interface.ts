476518f887079ce989a25d0a91edee1cc88fa8e1
EvgMel <melnikov_evg@mail.ru>
2021-08-04 11:55:03 +0300

    make a test change to check actions

--- src/config/interfaces/appConfig.interface.ts
+++ src/config/interfaces/appConfig.interface.ts
@@ -1,8 +1,10 @@
 import { AppEnv } from '../enums/appConfig.enum';
 import { HASHMAN_SECRET_KEY } from '../../hashman/interfaces/x-coder.interface';
 
+export const PORT = 'PORT';
+
 export default interface AppConfig {
   NODE_ENV: AppEnv;
-  PORT: number;
+  [PORT]: number;
   [HASHMAN_SECRET_KEY]: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import AppConfig from './config/interfaces/appConfig.interface';
+import AppConfig, { PORT } from './config/interfaces/appConfig.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config: ConfigService<AppConfig> = app.get(ConfigService);
 
-  await app.listen(config.get<number>('PORT'));
+  await app.listen(config.get<number>(PORT));
 }
 
 bootstrap();
